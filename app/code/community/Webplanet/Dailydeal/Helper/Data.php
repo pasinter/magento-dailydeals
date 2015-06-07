9d490eac8a85469e754f28764df1a2023e433b9b
pasinter <ken.golovin@gmail.com>
2015-06-08 08:43:08 +1200

    Helper - optimize date functions

--- app/code/community/Webplanet/Dailydeal/Helper/Data.php
+++ app/code/community/Webplanet/Dailydeal/Helper/Data.php
@@ -15,7 +15,27 @@
  */
 class Webplanet_Dailydeal_Helper_Data extends Mage_Core_Helper_Abstract
 {
-
+    /*
+     * var \DateTime
+     */
+    protected $currentStartTime;
+    
+    /*
+     * var \DateTime
+     */
+    protected $nextStartTime;
+    
+    /*
+     * var \DateTime
+     */
+    protected $expireTime;
+    
+    
+    /*
+     * var \DateTime
+     */
+    protected $todayExpireTime;
+    
     /**
      * Get prefix for all Dailydeal URLs
      *
@@ -26,48 +46,61 @@ class Webplanet_Dailydeal_Helper_Data extends Mage_Core_Helper_Abstract
         return Mage::getStoreConfig('catalog/dailydeal/url_prefix');
     }
 
+   
+    /**
+     * 
+     * @return \DateTime
+     */
     public function getCurrentStartTime()
     {
-        $expire_time = $this->getExpireTime();
+        if(null === $this->currentStartTime) {
+            $expire_time = $this->getExpireTime();
 
-        if ($this->getIsPastExpireTime()) {
-            $time_expire = Mage::app()->getLocale()
-                    ->date()
-                    ->setHour($expire_time->getHour())
-                    ->setMinute($expire_time->getMinute())
-                    ->setSecond($expire_time->getSecond());
-        } else {
-            $time_expire = Mage::app()->getLocale()
-                    ->date()
-                    ->sub(1, Zend_Date::DAY)
-                    ->setHour($expire_time->getHour())
-                    ->setMinute($expire_time->getMinute())
-                    ->setSecond($expire_time->getSecond());
+            if ($this->getIsPastExpireTime()) {
+                $this->currentStartTime = Mage::app()->getLocale()
+                        ->date()
+                        ->setHour($expire_time->getHour())
+                        ->setMinute($expire_time->getMinute())
+                        ->setSecond($expire_time->getSecond());
+            } else {
+                $this->currentStartTime = Mage::app()->getLocale()
+                        ->date()
+                        ->sub(1, Zend_Date::DAY)
+                        ->setHour($expire_time->getHour())
+                        ->setMinute($expire_time->getMinute())
+                        ->setSecond($expire_time->getSecond());
+            }
         }
-
-        return $time_expire;
+        
+        return $this->currentStartTime;
     }
 
+    /**
+     * 
+     * @return \DateTime
+     */
     public function getNextStartTime()
     {
-        $expire_time = $this->getExpireTime();
+        if(null === $this->nextStartTime) {
+            $expire_time = $this->getExpireTime();
 
-        if ($this->getIsPastExpireTime()) {
-            $time_expire = Mage::app()->getLocale()
-                    ->date()
-                    ->add(1, Zend_Date::DAY)
-                    ->setHour($expire_time->getHour())
-                    ->setMinute($expire_time->getMinute())
-                    ->setSecond($expire_time->getSecond());
-        } else {
-            $time_expire = Mage::app()->getLocale()
-                    ->date()
-                    ->setHour($expire_time->getHour())
-                    ->setMinute($expire_time->getMinute())
-                    ->setSecond($expire_time->getSecond());
+            if ($this->getIsPastExpireTime()) {
+                $this->nextStartTime = Mage::app()->getLocale()
+                        ->date()
+                        ->add(1, Zend_Date::DAY)
+                        ->setHour($expire_time->getHour())
+                        ->setMinute($expire_time->getMinute())
+                        ->setSecond($expire_time->getSecond());
+            } else {
+                $this->nextStartTime = Mage::app()->getLocale()
+                        ->date()
+                        ->setHour($expire_time->getHour())
+                        ->setMinute($expire_time->getMinute())
+                        ->setSecond($expire_time->getSecond());
+            }
         }
 
-        return $time_expire;
+        return $this->nextStartTime;
     }
 
     /**
@@ -77,15 +110,17 @@ class Webplanet_Dailydeal_Helper_Data extends Mage_Core_Helper_Abstract
      */
     public function getExpireTime()
     {
-        $config_time = Mage::getStoreConfig('catalog/dailydeal/expire_time');
+        if(null === $this->expireTime) {
+            $config_time = Mage::getStoreConfig('catalog/dailydeal/expire_time');
 
-        try {
-            $time = new Zend_Date($config_time, 'hh,mm,ss');
-        } catch (Exception $e) {
-            $time = new Zend_Date('23:59:59', 'hh:mm:ss');
+            try {
+                $this->expireTime = new Zend_Date($config_time, 'hh,mm,ss');
+            } catch (Exception $e) {
+                $this->expireTime = new Zend_Date('23:59:59', 'hh:mm:ss');
+            }
         }
 
-        return $time;
+        return $this->expireTime;
     }
 
     /**
@@ -125,15 +160,15 @@ class Webplanet_Dailydeal_Helper_Data extends Mage_Core_Helper_Abstract
      */
     public function getTodayExpireTime()
     {
-        $expire_time = $this->getExpireTime();
-
-
-        $today_time_expire = Mage::app()->getLocale()->date()
+        if(null === $this->todayExpireTime) {
+            $expire_time = $this->getExpireTime();
+            $this->todayExpireTime = Mage::app()->getLocale()->date()
                 ->setHour($expire_time->getHour())
                 ->setMinute($expire_time->getMinute())
                 ->setSecond($expire_time->getSecond());
-
-        return $today_time_expire;
+        }
+        
+        return $this->todayExpireTime;
     }
 
     /**
@@ -143,7 +178,6 @@ class Webplanet_Dailydeal_Helper_Data extends Mage_Core_Helper_Abstract
     public function getTomorrowExpireTime()
     {
         $expire_time = $this->getExpireTime();
-
 
         // the next expire time is tomorrow
         $tomorrow_time_expire = Mage::app()->getLocale()->date()
@@ -155,18 +189,15 @@ class Webplanet_Dailydeal_Helper_Data extends Mage_Core_Helper_Abstract
         return $tomorrow_time_expire;
     }
 
+    /**
+     * 
+     * @return boolean
+     */
     public function getIsPastExpireTime()
     {
         $today_time_expire = $this->getTodayExpireTime();
 
-        $store_date = Mage::app()->getLocale()->storeDate(null, null, true);
-
-        //echo "today_time_expire: $today_time_expire<br>";
-        //echo "store_date: $store_date<br>";
-        //echo '<br>' . $today_time_expire->compare($store_date) . '<br>'; exit;
-
         if ($today_time_expire->compare(time()) == 1) {
-
             return false;
         }
 
@@ -226,22 +257,6 @@ class Webplanet_Dailydeal_Helper_Data extends Mage_Core_Helper_Abstract
 
             return $deal;
         }
-
-        /*
-          $deal = Mage::getModel('dailydeal/deal')->loadByAttribute('product_id', $product->getEntityId());
-
-          $collection
-          ->addAttributeToFilter('product_id', $product->getEntityId())
-          ////->addAttributeToSelect('*')
-          //->setStoreId($storeId)
-          //->addStoreFilter($storeId)
-          //->setOrder('qty', 'desc')
-          ->setPageSize(1)
-          ;
-         * 
-         */
-
-        //$deal = $collection->
 
         return null;
     }
